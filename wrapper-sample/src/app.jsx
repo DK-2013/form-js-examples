a892ee5f8a5f1b059daf55a25bd8ece534d5a8a6
Кизилов Дмитрий <dkizilov@7tech.ru>
2023-11-10 17:25:33 +0300

    add playground

--- wrapper-sample/src/app.jsx
+++ wrapper-sample/src/app.jsx
@@ -1,11 +1,24 @@
 import { useEffect } from 'preact/hooks';
-// import { FormPlayground } from '@bpmn-io/form-js';
-import './app.css'
+import { FormPlayground } from '@bpmn-io/form-js';
+
+import '@bpmn-io/form-js/dist/assets/form-js.css';
+import '@bpmn-io/form-js/dist/assets/form-js-editor.css';
+import '@bpmn-io/form-js/dist/assets/form-js-playground.css';
+
+const schema = {
+  "type": "default",
+  "components": []
+};
 
 export function App() {
   useEffect(() => {
     const container = document.getElementById('anchor');
     console.log(container);
+    new FormPlayground({
+      container,
+      schema,
+      data: {},
+    });
   }, []);
   return (
     <div id="anchor" />
