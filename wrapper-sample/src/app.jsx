6709414b770832bd19bed12ab90d24c04189e9f1
Кизилов Дмитрий <dkizilov@7tech.ru>
2023-11-10 17:17:52 +0300

    check container

--- wrapper-sample/src/app.jsx
+++ wrapper-sample/src/app.jsx
@@ -3,7 +3,10 @@ import { useEffect } from 'preact/hooks';
 import './app.css'
 
 export function App() {
-
+  useEffect(() => {
+    const container = document.getElementById('anchor');
+    console.log(container);
+  }, []);
   return (
     <div id="anchor" />
   )
